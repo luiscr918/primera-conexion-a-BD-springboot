dd57ba37512dcee032ff705da32b70d1082313d4
luiscr918 <lc9107040@gmail.com>
2025-06-12 08:04:22 -0500

    opcion de guardar lista de libros como pdf lista

--- src/main/java/com/itsqmet/controller/LibroControlador.java
+++ src/main/java/com/itsqmet/controller/LibroControlador.java
@@ -1,16 +1,22 @@
 package com.itsqmet.controller;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.itsqmet.entity.Autor;
 import com.itsqmet.entity.Genero;
 import com.itsqmet.entity.Libro;
 import com.itsqmet.service.AutorService;
 import com.itsqmet.service.GeneroService;
 import com.itsqmet.service.LibroServicio;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -64,6 +70,22 @@ public class LibroControlador {
     public String eliminarLibro(@PathVariable Long id){
          libroServicio.eliminarLibro(id);
          return "redirect:/libros";
+    }
+    //metodo para pdf
+    @GetMapping("/librosPDF")
+    public void exportarPDF(HttpServletResponse response )throws IOException, DocumentException {
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=libros.pdf");
+        List<Libro> libros = libroServicio.mostrarLibros();
+        Document documento = new Document();
+        PdfWriter.getInstance(documento, response.getOutputStream());
+        documento.open();
+        documento.add(new Paragraph("Listado de Libros"));
+        documento.add(new Paragraph(" "));
+        for (Libro libro : libros ){
+            documento.add(new Paragraph(libro.getId() + " - " + libro.getTitulo() + " - " + libro.getAutor().getNombre()));
+        }
+        documento.close();
     }
 
 }
